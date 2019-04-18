d065b2656068ea042d312843da8b69b5a680e5a9
atsekoff <atsekoff@gmail.com>
2019-04-18 21:40:21 +0200

    Driver installer

---
+++ Lib/InterceptionDriver/InstallDriver.bat
@@ -0,0 +1,28 @@
+@echo off
+if _%1_==_payload_  goto :payload
+
+:getadmin
+    echo %~nx0: elevating self
+    set vbs=%temp%\getadmin.vbs
+    echo Set UAC = CreateObject^("Shell.Application"^)                >> "%vbs%"
+    echo UAC.ShellExecute "%~s0", "payload %~sdp0 %*", "", "runas", 1 >> "%vbs%"
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+goto :eof
+
+:payload
+
+::ENTER YOUR CODE BELOW::   
+
+
+"%~dp0\command line installer\install-interception.exe" /install
+
+
+
+::END OF YOUR CODE::
+
+echo.
+echo...Script Complete....
+echo.
+
+pause
